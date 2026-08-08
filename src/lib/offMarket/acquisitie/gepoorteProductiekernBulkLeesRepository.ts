8472a6ad7cc2f500b93ddf39e293e3e0c9c3288e
rlsbito90-eng <rlsbito90@gmail.com>
2026-08-08 16:49:29 +0200

    feat(acquisitie): poort bulkreads met centrale leesactivatie

---
+++ src/lib/offMarket/acquisitie/gepoorteProductiekernBulkLeesRepository.ts
@@ -0,0 +1,31 @@
+import type { ProductieLeesActivatieBesluit } from './productieLeesActivatiePoort';
+import { ProductiekernNietGeactiveerdError } from './productiekernRepository';
+import type { ProductiekernBulkLeesRepository } from './productiekernSupabaseBulkLeesRepository';
+
+export class GepoorteProductiekernBulkLeesRepository implements ProductiekernBulkLeesRepository {
+  constructor(
+    private readonly activatie: ProductieLeesActivatieBesluit,
+    private readonly achterliggend: ProductiekernBulkLeesRepository,
+  ) {}
+
+  private eisLeestoegang(handeling: string): void {
+    if (!this.activatie.lezenActief) throw new ProductiekernNietGeactiveerdError(handeling);
+  }
+
+  haalBrievenOpIds(ids: readonly string[]) {
+    this.eisLeestoegang('haalBrievenOpIds');
+    return this.achterliggend.haalBrievenOpIds(ids);
+  }
+
+  haalBriefversiesOpIds(ids: readonly string[]) {
+    this.eisLeestoegang('haalBriefversiesOpIds');
+    return this.achterliggend.haalBriefversiesOpIds(ids);
+  }
+}
+
+export function maakGepoorteProductiekernBulkLeesRepository(
+  activatie: ProductieLeesActivatieBesluit,
+  achterliggend: ProductiekernBulkLeesRepository,
+): ProductiekernBulkLeesRepository {
+  return new GepoorteProductiekernBulkLeesRepository(activatie, achterliggend);
+}
