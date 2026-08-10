beee5540aa124ea575cbcd0e9e1cd043dd1fac20
rlsbito90-eng <rlsbito90@gmail.com>
2026-08-11 01:39:47 +0200

    feat(navigation): wrap core CRM detail routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ScrollToTop from "@/components/ScrollToTop";
 import DynamicSectionNavigator from "@/components/DynamicSectionNavigator";
+import CrmDetailNavigationBoundary from "@/components/CrmDetailNavigationBoundary";
 import { AuthProvider } from "@/hooks/useAuth";
 import { DataStoreProvider, useDataStore } from "@/hooks/useDataStore";
 import { SubcategorieProvider } from "@/hooks/useSubcategorieen";
@@ -90,11 +91,32 @@ const App = () => (
                         <Routes>
                           <Route path="/" element={<DashboardPage />} />
                           <Route path="/relaties" element={<RelatiesPage />} />
-                          <Route path="/relaties/:id" element={<RelatieDetailPage />} />
+                          <Route
+                            path="/relaties/:id"
+                            element={
+                              <CrmDetailNavigationBoundary fallbackPath="/relaties" fallbackLabel="Relaties" source="relatie-detail">
+                                <RelatieDetailPage />
+                              </CrmDetailNavigationBoundary>
+                            }
+                          />
                           <Route path="/objecten" element={<ObjectenPage />} />
-                          <Route path="/objecten/:id" element={<SafeObjectDetailRoute />} />
+                          <Route
+                            path="/objecten/:id"
+                            element={
+                              <CrmDetailNavigationBoundary fallbackPath="/objecten" fallbackLabel="Objecten" source="object-detail">
+                                <SafeObjectDetailRoute />
+                              </CrmDetailNavigationBoundary>
+                            }
+                          />
                           <Route path="/deals" element={<DealsPage />} />
-                          <Route path="/deals/:id" element={<DealDetailPage />} />
+                          <Route
+                            path="/deals/:id"
+                            element={
+                              <CrmDetailNavigationBoundary fallbackPath="/deals" fallbackLabel="Deals" source="deal-detail">
+                                <DealDetailPage />
+                              </CrmDetailNavigationBoundary>
+                            }
+                          />
                           <Route path="/zoekprofielen" element={<ZoekprofielenPage />} />
                           <Route path="/pipeline" element={<PipelinePage />} />
                           <Route path="/acquisitie" element={<AcquisitiePage />} />
@@ -106,10 +128,24 @@ const App = () => (
                           <Route path="/vastgoedkansen/pandcheck" element={<SnellePandcheckPage />} />
                           <Route path="/vastgoedkansen/:id" element={<VastgoedkansDetailPage />} />
                           <Route path="/off-market" element={<OffMarketPage />} />
-                          <Route path="/off-market/:id" element={<OffMarketSignaalRoutePage />} />
+                          <Route
+                            path="/off-market/:id"
+                            element={
+                              <CrmDetailNavigationBoundary fallbackPath="/off-market" fallbackLabel="Off-Market Radar" source="off-market-detail">
+                                <OffMarketSignaalRoutePage />
+                              </CrmDetailNavigationBoundary>
+                            }
+                          />
                           <Route path="/referentieobjecten" element={<ReferentieObjectenPage />} />
                           <Route path="/taken" element={<TakenPage />} />
-                          <Route path="/taken/:id" element={<TaakDetailPage />} />
+                          <Route
+                            path="/taken/:id"
+                            element={
+                              <CrmDetailNavigationBoundary fallbackPath="/taken" fallbackLabel="Taken" source="taak-detail">
+                                <TaakDetailPage />
+                              </CrmDetailNavigationBoundary>
+                            }
+                          />
                           <Route path="/vastgoedrekenen" element={<VastgoedrekenenPage />} />
                           <Route path="/rapportage" element={<RapportagePage />} />
                           <Route path="/rapportage/kadasterkosten" element={<KadasterKostenPage />} />
