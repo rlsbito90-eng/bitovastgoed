206c4fe4ad6282d9efcf3f1a55fd173b69dced12
rlsbito90-eng <rlsbito90@gmail.com>
2026-08-15 17:52:27 +0200

    TEST MIG: extend remote hash recovery proof

--- src/test/migratie/crmMigrationHistoryRecovery.test.ts
+++ src/test/migratie/crmMigrationHistoryRecovery.test.ts
@@ -7,6 +7,9 @@ const RECOVERED_MIGRATIONS = [
   ['20260809141031_crm_mig_contact_moments.sql', '27cb5e09825f31522daaed336337ad4b'],
   ['20260809141217_crm_mig_off_market_ai_fields.sql', '88b6a91304d7670f3deccf8e4c1b1c24'],
   ['20260809141258_crm_mig_off_market_bron_stats.sql', '2eba5b1efe9d20084e385aaf2f6a2b7a'],
+  ['20260809141459_crm_mig_off_market_import_contract.sql', '798ab1ee635c0882983492bd3a4db96a'],
+  ['20260809141552_crm_mig_off_market_brief_acquisitie.sql', '5f205742756bebff24a1b98e19fc54f2'],
+  ['20260809141657_crm_mig_off_market_promote_rpc.sql', '77f73110632b8839ac0fb847aa9c1906'],
 ] as const;
 
 describe('CRM migration history recovery', () => {
