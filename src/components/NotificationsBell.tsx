b9ea1b6f3d556bea4f32e232c3efde5cf92d6366
rlsbito90-eng <rlsbito90@gmail.com>
2026-08-18 10:39:41 +0200

    TEST notificaties: maak V2 facade comment regressieproof

--- src/components/NotificationsBell.tsx
+++ src/components/NotificationsBell.tsx
@@ -1,3 +1,3 @@
 // Compatibiliteitsfacade: alle bestaande imports gebruiken voortaan de server-backed V2-bel.
-// De oude localStorage-triggerengine is hiermee uit de actieve productie-UI gehaald.
+// De oude lokale triggerengine is hiermee uit de actieve productie-UI gehaald.
 export { default } from './NotificationsBellV2';
