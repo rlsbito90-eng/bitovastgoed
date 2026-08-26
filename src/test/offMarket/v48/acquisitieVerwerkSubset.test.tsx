6d7081c8a169335643f29e58fda34f5b1c140179
rlsbito90-eng <rlsbito90@gmail.com>
2026-08-26 19:21:48 +0200

    Completeer werkvoorraadmock in V48

--- src/test/offMarket/v48/acquisitieVerwerkSubset.test.tsx
+++ src/test/offMarket/v48/acquisitieVerwerkSubset.test.tsx
@@ -49,6 +49,13 @@ const mockSignalen = [
 ];
 
 vi.mock('@/hooks/useAcquisitieSelectie', () => ({
+  WERKVOORRAAD_STATUS_LABEL: {
+    actief: 'Actief',
+    gebundeld_bij_partij: 'Gebundeld bij partij',
+    eerder_benaderd: 'Eerder benaderd',
+    benadering_bepalen: 'Benadering bepalen',
+    niet_benaderen: 'Niet benaderen',
+  },
   useAcquisitieSelectie: () => ({
     data: mockSignalen.map((s, i) => ({
       id: `r${i}`, signaal_id: s.id, archived_at: null,
